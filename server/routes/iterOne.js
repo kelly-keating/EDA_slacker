5d7c2cd011c22fca3465921ead4edc3f74f01068
kelly-keating <keating.kel@gmail.com>
2018-07-16 10:12:37 +1200

    console log added

--- server/routes/iterOne.js
+++ server/routes/iterOne.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', (req, res) => {
-  res.status(200).json({ challenge: req.body.challenge });
+  if(req.body.challenge){
+    res.status(200).json({ challenge: req.body.challenge });
+  } else {
+    console.log(req.body)
+  }
 })
 
 module.exports = router
