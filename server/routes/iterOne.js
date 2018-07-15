f10fe80fea855eda5bb0673bd02948fda00631d7
kelly-keating <keating.kel@gmail.com>
2018-07-16 10:06:13 +1200

    challenge from body

--- server/routes/iterOne.js
+++ server/routes/iterOne.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', (req, res) => {
-  res.status(200).json({ challenge: req.challenge });
+  res.status(200).json({ challenge: req.body.challenge });
 })
 
 module.exports = router
